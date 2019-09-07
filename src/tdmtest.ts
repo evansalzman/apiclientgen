66d2b6143566443bf9ab5d31efa896aa60d632f8
robert arles <robert.r.arles@disney.com>
2019-09-06 21:21:07 -0700

    Working test use of GetTdmTables(accessToken)

--- src/tdmtest.ts
+++ src/tdmtest.ts
@@ -1,14 +1,14 @@
-// import { TdmClient } from './TdmClient/TdmClient';
+import { TdmClient } from './TdmClient/TdmClient';
 
-// const basePath: string = 'https://stage.tdmtables.wdprapps.disney.com';
-// const apiUri: string = '/rest/v1';
-// const accessToken: string = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzY29wZSI6WyJwcm9maWxlIl0sImlzcyI6InRkbS53ZHcuZGlzbmV5LmNvbSIsInRkbVVzZXJSb2xlcyI6WyJST0xFX1RETV9UQUJMRV9VU0VSIiwiUk9MRV9FWFRfVE9LRU5fVVNSIiwiUk9MRV9URE9EX1dPUktGTE9XX0FETUlOIl0sImV4cCI6MTU2NzgwOTk4OSwiaWF0IjoxNTY3Nzg4Mzg5LCJ1c2VybmFtZSI6ImYtYXV0b2Z4In0.9oV0_dyS8pFGZKh5SOLjKau_yiG2h5kmrlfpMBTDGRI';
+const basePath: string = 'https://stage.tdmtables.wdprapps.disney.com';
+const apiUri: string = '/rest/v1';
+const accessToken: string = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzY29wZSI6WyJwcm9maWxlIl0sImlzcyI6InRkbS53ZHcuZGlzbmV5LmNvbSIsInRkbVVzZXJSb2xlcyI6WyJST0xFX1RETV9UQUJMRV9VU0VSIiwiUk9MRV9FWFRfVE9LRU5fVVNSIiwiUk9MRV9URE9EX1dPUktGTE9XX0FETUlOIl0sImV4cCI6MTU2NzgzMTM3MCwiaWF0IjoxNTY3ODA5NzcwLCJ1c2VybmFtZSI6ImYtYXV0b2Z4In0.PCfLJFshH2lS7UB4Ap87VKSXmx8LsDSdKzWR66iDCJM';
 
-// const tdm = new TdmClient (`${basePath}${apiUri}`);
+const tdm = new TdmClient (`${basePath}${apiUri}`);
 
-// async function testModule () {
-//   const result = await tdm.GetTdmtables ();
-//   console.log (`DEBUG -- result ${require ('util').inspect (result, {colors: true, depth: 2})}`);
-// }
+async function testModule () {
+  const result = await tdm.GetTdmtables (`Bearer ${accessToken}`);
+  console.log (`DEBUG -- result ${require ('util').inspect (result, {colors: true, depth: 2})}`);
+}
 
-// testModule ();
+testModule ();
